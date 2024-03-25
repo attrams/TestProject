d9507cea5aa8583316c57d350ee1ccb4adb20c3f
attrams <48452451+Attrams@users.noreply.github.com>
2024-03-25 01:10:52 +0000

    comments lesson

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
-﻿string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+﻿/* 
+    The following code creates five random OrderIDs to test the fraud detection process.
+    OrderIDs consist of a letter from A to E, and a three digit number. Ex. A123.
+ */
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
+{
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
 
 foreach (string orderID in orderIDs)
 {
-    if (orderID.StartsWith("B"))
-    {
-        Console.WriteLine(orderID);
-    }
+    Console.WriteLine(orderID);
 }
