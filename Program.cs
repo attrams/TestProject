f96ef38b1048ed411ef0ccf1462711133d904199
attrams <48452451+Attrams@users.noreply.github.com>
2024-03-26 16:10:18 +0000

    complete FizzBuzz challenge

--- Program.cs
+++ Program.cs
@@ -1,6 +1,22 @@
-﻿string[] names = { "Alex", "Eddie", "David", "Michael" };
-
-for (int i = names.Length - 1; i >= 0; i--)
+﻿for (int i = 1; i < 101; i++)
 {
-    Console.WriteLine(names[i]);
+    if ((i % 3 == 0) || (i % 5 == 0))
+    {
+        if ((i % 3 == 0) && (i % 5 == 0))
+        {
+            Console.WriteLine($"{i} - FizzBuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            Console.WriteLine($"{i} - Fizz");
+        }
+        else
+        {
+            Console.WriteLine($"{i} - Buzz");
+        }
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
 }
