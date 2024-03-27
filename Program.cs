0c7e070e3ab59825b6c9e1979dfe43d74e049edb
attrams <48452451+Attrams@users.noreply.github.com>
2024-03-27 17:44:27 +0000

    complete the types lesson

--- Program.cs
+++ Program.cs
@@ -1,22 +1,25 @@
-﻿string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices."];
+﻿/* Value types */
+Console.WriteLine("Signed integral types: ");
 
-foreach (string myString in myStrings)
-{
-    int periodLocation = myString.IndexOf(".");
-    string newString = myString;
-    string outputString;
+Console.WriteLine($"sbyte : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int   : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long  : {long.MinValue} to {long.MaxValue}");
 
-    while (periodLocation != -1)
-    {
-        outputString = newString.Remove(periodLocation);
-        newString = newString.Substring(periodLocation + 1).TrimStart();
-        periodLocation = newString.IndexOf(".");
-        Console.WriteLine(outputString);
-    }
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types: ");
 
-    if (newString.Length > 0)
-    {
-        Console.WriteLine(newString.Trim());
-    }
-}
-// Console.WriteLine(myStrings[0].Substring(myStrings[0].IndexOf(".") + 1));
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+
+Console.WriteLine($"float   : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double  : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal : {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+/* Reference types */
+int[] data = new int[3];
