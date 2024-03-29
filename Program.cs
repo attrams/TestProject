261f89c58b41817e8f369f66cf746369882e9fae
attrams <48452451+Attrams@users.noreply.github.com>
2024-03-29 12:49:03 +0000

    complete string interpolation exercise

--- Program.cs
+++ Program.cs
@@ -1,22 +1,11 @@
-﻿/* Composite formatting */
-using System.Globalization;
+﻿int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
 
-string first = "Hello";
-string second = "World";
-
-Console.WriteLine("{1} {0}!", first, second);
-Console.WriteLine("{0} {0} {0}!", first, second);
-
-/* Formatting currency */
-decimal price = 123.45m;
-int discount = 50;
-
-Console.WriteLine($"Price: {price:C} (save {discount:C})");
-
-/* Formatting numbers */
-decimal measurement = 123456.78912m;
-Console.WriteLine($"Measurement: {measurement:N4} units");
-
-/* Formatting percentages */
-decimal tax = .36785m;
-Console.WriteLine($"Tax rate: {tax:P1}");
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
